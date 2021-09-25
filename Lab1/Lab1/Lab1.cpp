a4bfc0b60dd35ea43a03ed0f74df6f891e94b66e
Erkin Kamilov <erkimkamilov@gmail.com>
2021-09-26 02:46:32 +0500

    Added 3 tests. Manual print.

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -4,17 +4,36 @@
 #include <iostream>
 #include <omp.h>
 
+
+void printArray(float const *const *const arr, int const m, int const n)
+{
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+        {
+            std::cout << "  " << arr[i][j];
+        }
+        std::cout << "\n";
+    }
+}
+
+void printVector(float const *const vector, int const m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        std::cout << "  " << vector[i] << "\n";
+    }
+}
+
+
 /// <summary>
 /// Возвращает двумерный массив со случайными целыми числами в диапазоне [-10; 10].
 /// </summary>
 /// <param name="m"> Число строк </param>
 /// <param name="n"> Число столбцов </param>
 /// <returns> Указатель на массив </returns>
-float** initRandom2DArray(int m, int n)
+float** initRandom2DArray(int const m, int const n)
 {
     float** arr = new float* [m];
-
-    std::cout << "\nДвумерный массив:\n";
 
     for (int i = 0; i < m; i++) {
         arr[i] = new float[n];
@@ -22,9 +41,7 @@ float** initRandom2DArray(int m, int n)
         for (int j = 0; j < n; j++)
         {
             arr[i][j] = rand() % 21 - 10;
-            std::cout << "  " << arr[i][j];
         }
-        std::cout << "\n";
     }
 
     return arr;
@@ -35,27 +52,16 @@ float** initRandom2DArray(int m, int n)
 /// </summary>
 /// <param name="m"> Размерность вектора </param>
 /// <returns> Указатель на вектор </returns>
-float* initRandom(int m)
+float* initRandom(int const m)
 {
     float* vector = new float[m];
-
-    std::cout << "\nВектор:\n";
 
     for (int i = 0; i < m; i++)
     {
         vector[i] = rand() % 21 - 10;
-        std::cout << "  " << vector[i] << "\n";
     }
 
     return vector;
-}
-
-void printVector(float* vector, int m)
-{
-    for (int i = 0; i < m; i++)
-    {
-        std::cout << "  " << vector[i] << "\n";
-    }
 }
 
 
@@ -63,21 +69,31 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     int m, n;
+    bool isPrintData = true;
+    
     std::cout << "Введите размерность матрицы (m x n).\n";
     std::cout << "m = ";
     std::cin >> m;
     std::cout << "n = ";
     std::cin >> n;
-    std::cout << "Размерность вектора = m (m = " << m << " )\n";
+    std::cout << "Размерность вектора = n (n = " << n << " )\n";
+    std::cout << "Заполнение массива случайными числами...\n";
+
+#pragma region Arrays initialization
 
     srand(time(0));
     float** arr = initRandom2DArray(m, n);
-    float* vector = initRandom(m);
+    float* vector = initRandom(n);
     float* resultVector = new float[m];
 
-    double startTime = clock();
+#pragma endregion
 
-#pragma omp parallel for shared(arr, vector, resultVector, n, m) // попробовать firstprivate(n, m), согласно https://docs.oracle.com/cd/E19059-01/stud.10/819-0501/7_tuning.html
+
+#pragma region Matrix product
+
+    double singleThreadStartTime = clock();
+
+    // Умножение в одном потоке.
     for (int i = 0; i < m; i++)
     {
         resultVector[i] = 0;
@@ -87,12 +103,55 @@ int main()
         }
     }
 
-    double endTime = clock();
-    double resultTime = (endTime - startTime) / CLOCKS_PER_SEC;
+    double singleThreadEndTime = clock() - singleThreadStartTime;
+    double multiThreadStartTime = clock();
+
+    // Умножение в m потоках openMP.
+#pragma omp parallel for shared(arr, vector, resultVector, m, n)
+    for (int i = 0; i < m; i++)
+    {
+        resultVector[i] = 0;
+        for (int j = 0; j < n; j++)
+        {
+            resultVector[i] += arr[i][j] * vector[j];
+        }
+    }
+
+    double multiThreadEndTime = clock() - multiThreadStartTime;
+    double multiThreadStartTime2 = clock();
+
+    // Умножение в m потоках openMP с директивой firstprivate для констант (оптимизация предложена в https://docs.oracle.com/cd/E19059-01/stud.10/819-0501/7_tuning.html).
+#pragma omp parallel for shared(arr, vector, resultVector) firstprivate(m, n)
+    for (int i = 0; i < m; i++)
+    {
+        resultVector[i] = 0;
+        for (int j = 0; j < n; j++)
+        {
+            resultVector[i] += arr[i][j] * vector[j];
+        }
+    }
+
+    double multiThreadEndTime2 = clock() - multiThreadStartTime2;
+
+#pragma endregion
 
 
-    std::cout << "Результат:\n";
-    printVector(resultVector, m);
-    std::cout << "Затраченное время: " << resultTime << " сек.";
+    std::cout << "Single-thread, затраченное время: " << singleThreadEndTime / CLOCKS_PER_SEC << " сек.\n";
+    std::cout << "Multi-thread, затраченное время: " << multiThreadEndTime / CLOCKS_PER_SEC << " сек.\n";
+    std::cout << "Multi-thread + firstprivate(m, n), затраченное время: " << multiThreadEndTime2 / CLOCKS_PER_SEC << " сек.\n";
+
+    
+    std::cout << "Показать данные? (1 - да, 0 - нет)\n";
+    std::cin >> isPrintData;
+    if (isPrintData)
+    {
+        std::cout << "\nДвумерный массив:\n";
+        printArray(arr, m, n);
+        std::cout << "\nВектор:\n";
+        printVector(vector, n);
+        std::cout << "Результат умножения:\n";
+        printVector(resultVector, m);
+    }
+    
     std::cin;
 }
